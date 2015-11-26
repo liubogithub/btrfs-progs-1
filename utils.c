db8bbf3d0dd6306ad2eb4213fb65c022d4b76f59
David Sterba <dsterba@suse.com>
2015-11-26 17:56:45 +0100

    btrfs-progs: docs: mkfs, implications of DUP on devices
    We offer DUP but still depend on the hardware, to do the right thing.
    
    Signed-off-by: David Sterba <dsterba@suse.com>

--- utils.c
+++ utils.c
@@ -2504,8 +2504,9 @@ int test_num_disk_vs_raid(u64 metadata_profile, u64 data_profile,
 		return 1;
 	}
 
-	warning_on(!mixed && (data_profile & BTRFS_BLOCK_GROUP_DUP) && ssd,
-		   "DUP have no effect if your SSD have deduplication function");
+	/* warning_on(!mixed && (data_profile & BTRFS_BLOCK_GROUP_DUP) && ssd, */
+	warning_on(!mixed && (data_profile & BTRFS_BLOCK_GROUP_DUP),
+		   "DUP may not actually lead to 2 copies on the device, see manual page");
 
 	return 0;
 }
